a6544d7614aba6f66bd0b59c0eed8e9899da28db
ervin-wang <orange.work@qq.com>
2014-11-14 00:33:59 +0800

    工程转移

--- library/src/com/ervinwang/bthelper/core/IReceiveData.java
+++
@@ -1,7 +0,0 @@
-package com.ervinwang.bthelper.core;
-
-public interface IReceiveData {
-
-	void receiveData(int length, byte[] buffer);
-
-}
--- library/src/com/ervinwang/bthelper/core/OnDeviceStateChangeListener.java
+++
@@ -1,7 +0,0 @@
-package com.ervinwang.bthelper.core;
-
-public interface OnDeviceStateChangeListener {
-
-	public void onDeviceStateChange(int state);
-
-}
